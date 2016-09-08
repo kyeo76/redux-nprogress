729ee04473008444f5d2ab40aa23d070aa80b177
jaredt67 <jaredt67@gmail.com>
2016-09-09 01:38:37 +0300

    safari fix

--- example/src/actions/gifs.js
+++ example/src/actions/gifs.js
@@ -3,11 +3,15 @@ import * as ActionTypes from 'constants/gifs';
 import { beginTask, endTask } from '../../../src';
 
 export function getRandomGif() {
+  const screwSafari = Math.random().toString(36).substr(2);
+
   return {
     type: ActionTypes.GET_RANDOM_GIF,
     nprogress: true,
-    payload: fetch('https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC').then(res => res.json())
-  }
+    payload: fetch(
+      `https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&screw_safari=${screwSafari}`
+    ).then(res => res.json())
+  };
 }
 
 export function loadMultipleGifs(n = 1) {
