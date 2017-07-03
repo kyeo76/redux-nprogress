76863273755d5a991b5e1491e74ae1532d558a33
Jeffrey Hoffer <jhoffer@aktarytech.com>
2017-07-02 23:02:15 -0700

    install prop-types package as dependency change all instances of React.PropTypes to use from prop-types package Updated peerDepency on react & react-dom to ^0.14.9 Added babel-cli as devDependency for building

--- example/src/containers/App.js
+++ example/src/containers/App.js
@@ -1,4 +1,5 @@
-import React, { PropTypes, Component } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { StickyContainer } from 'react-sticky';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
